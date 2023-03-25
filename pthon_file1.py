97d17b764581d891ee94439efff774ea0a2641d1
prasanna <bhursuprasannakumar@gmail.com>
2023-03-25 10:34:25 +0530

    multiple inputs added
    Here we added multile input files inorder to contribute  to open source projecs

--- pthon_file1.py
+++ pthon_file1.py
@@ -1,4 +1,9 @@
 print("hello")
 n=int(input())
+n1=int(input("Enter the first number"))
 m=int(input())
+m1=int(input("Enter the seocnd number"))
 print("n+m=",n+m)
+print("n1+m1",n1+m1)
+
+print("pythno-files")
